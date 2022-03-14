b6877fb5fc431c15d77086e10a797bc78c2b48fc
Anuhya BS <anuhyabs@gmail.com>
2022-03-13 17:47:58 -0700

    Updating setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     ],
     keywords='python, wordle',
     packages=find_packages(),
+    python_requires='>=3.6',
     install_requires=['pandas', 'scipy', 'pickle', 'itertools',
 	'Counter', 'tweepy', 'pytz', 'datetime'
 ],)
